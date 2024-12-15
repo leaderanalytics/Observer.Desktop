80f8bf75aed38e74f45bdbc1c01228fd1fb63293
Sam Wheat <sam.wheat@outlook.com>
2024-12-14 20:33:24 -0800

    Delete unused clickonce files

--- ClickOnce.bat
+++
@@ -1,3 +0,0 @@
-dotnet mage -al Observer.exe -td Observer.Desktop\bin\Release\net8
-dotnet mage -new Application -t  Observer.Desktop\bin\Release\net8\Observer.Manifest -fd Observer.Desktop\bin\Release\net8 -v 1.0.0.0
-dotnet mage -new Deployment -Install true -pub "Leader Analytics" -v 1.0.0.0 -AppManifest Observer.Desktop\bin\Release\net8\Observer.manifest -t Observer.application
